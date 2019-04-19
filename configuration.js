18292143d639fbe1ac05fc41cb12aed66e342ccc
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-04-19 20:36:54 +0200

    Add configuration file an change .env values

---
+++ configuration.js
@@ -0,0 +1,4 @@
+var gotoTimeout = process.env.GO_TO_TIMEOUT;  // Los ms a esperar para cargar páginas
+var hideBrowser = process.env.HIDE_BROWSER;
+var isRaspberry = process.env.RASPBERRY;
+var duration_site = process.env.DURATION_SITE;  // Duración en cada sitio web
