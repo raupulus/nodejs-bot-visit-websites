486a274dfab8d226ec99cabfffdce082f18f10f9
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-04-19 21:33:27 +0200

    Create visit for all sites

--- index.js
+++ index.js
@@ -13,8 +13,46 @@ imports( 'configuration' );
  */
 imports( 'functions' );
 
-console.log('Iniciando Bot para pasear por los sitios webs');
+/**
+ * Import configuration for all sites. Array with objects.
+ */
+var all_sites = require('./sites.json');
+
+log('Iniciando Bot para pasear por los sitios webs');
 
 /*****************************************************
-                  Flujo Principal
+ **                Flujo Principal                  **
  *****************************************************/
+
+function init() {
+    //console.log(all_sites);
+
+    all_sites.forEach((ele) => {
+        visitWebsite(ele);
+    });
+}
+
+init();
+
+async function visitWebsite(configSite) {
+    log(configSite);
+
+    // Inicio el navegador y realizo el login.
+    log('Open Browser');
+    const browser = await initBrowser(puppeteer, hideBrowser, isRaspberry);
+    const page = await initPage(browser);
+
+    // Si existe login se realiza
+    if (configSite.url_login) {
+        log('Realizando proceso de Login en ' + configSite.url_login);
+        await login(page, configSite.url_login);
+        sleep.sleep(getRandomInt(1, 5));
+    }
+
+    // Visit the main website.
+    log('Go to ' + configSite.url_web);
+    await goToPage(page, configSite.url_web);
+    sleep.sleep(getRandomInt(3, 13));
+
+    await browser.close();
+}
